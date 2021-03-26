1d279e17636461484e2b8bd136cb7f7ae595d2d3
Michael Max <mikedanzon@gmail.com>
2021-03-26 13:30:15 +0700

    codewars

--- personal/latihan-javascript/codewars.js
+++ personal/latihan-javascript/codewars.js
@@ -1,21 +1,35 @@
-function xMasTree(n){
+// function xMasTree(n){
+//     let output = "";
+//     for (var i = 0; i < n; i++) {
+//         for (var j = i; j < n; j++) {
+//             output += "_";
+//         }
+//         for (var k = 0; k < i; k++) {
+//             output += "#";
+//         }
+//         for (var l = 0; l < j; l++) {
+//             output += "_";
+//         }
+//         for (var m = 0; m < k; m++) {
+//             output += "#";
+//         }
+//         output += "\n";
+//     }
+//     return output;
+// }
+
+// console.log(xMasTree(5))
+
+function accum(s) {
     let output = "";
-    for (var i = 0; i < n; i++) {
-        for (var j = i; j < n; j++) {
-            output += "_";
+    for (let i = 0; i < s.length; i++) {
+        for (let j = 0; j <= i; j++) {
+            output += s[i];
         }
-        for (var k = 0; k < i; k++) {
-            output += "#";
-        }
-        for (var l = 0; l < j; l++) {
-            output += "_";
-        }
-        for (var m = 0; m < k; m++) {
-            output += "#";
-        }
-        output += "\n";
+        output += "-";
     }
-    return output;
+    let result = output.toLowerCase().split("-").join("-");
+    return result;
 }
 
-console.log(xMasTree(5))
+console.log(accum("ZpglnRxqenU"));
